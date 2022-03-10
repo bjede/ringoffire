84b0c58151a2217fa1efed6efcc0193688fdc180
Aldin <aldin.bijedic@gmx.at>
2022-03-10 23:49:24 +0100

    Added gameId and removed currentCard

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
 
 export interface DialogData {
   name: string;
@@ -14,61 +15,89 @@ export interface DialogData {
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-
-  pickCardAnimation: boolean = false;
   game: Game = new Game();
-  currentCard: any;
   name: string = '';
+  gameId!: string;
 
 
 
-  constructor(private firestore: AngularFirestore, public dialog: MatDialog) { }
+  constructor(private route: ActivatedRoute,
+    private firestore: AngularFirestore,
+    public dialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.newGame();
-    this.firestore.collection('games')
-    .valueChanges().subscribe((game) => {
-      console.log(game);
+    //this.newGame();
+    this.route.params.subscribe((params: any) => {
+      this.gameId = params.id;
+      this.firestore
+        .collection('games')
+        .doc(params.id)
+        .valueChanges()
+        .subscribe((game: any) => {
+          this.game.currentPlayer = game.currentPlayer;
+          this.game.playedCards = game.playedCards;
+          this.game.players = game.players;
+          this.game.stack = game.stack;
+          this.game.pickCardAnimation = game.pickCardAnimation;
+          this.game.currentCard = game.currentCard;
+
+          console.log(game);
+        });
     });
-    console.log(this.game.stack);
   }
 
   newGame() {
     this.game = new Game();
-    this.firestore
-    .collection('games')
-    .add({'Card': this.currentCard});
-    console.log(this.game);
   }
 
   takeCard() {
 
-    if (!this.pickCardAnimation) {
-      this.currentCard = this.game.stack.pop();
-      this.pickCardAnimation = true;
+    if (!this.game.pickCardAnimation && this.game.players.length > 0) {
+      this.game.currentCard = this.game.stack.pop();
+      this.game.pickCardAnimation = true;
       console.log(this.game.playedCards);
+
+      this.game.currentPlayer++
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+
+      this.saveGame();
 
 
       setTimeout(() => {
-        this.game.playedCards.push(this.currentCard);
-        this.pickCardAnimation = false;
+        this.game.playedCards.push(this.game.currentCard);
+
+        this.game.pickCardAnimation = false;
+        this.saveGame();
       }, 1500);
+
     }
-    this.game.currentPlayer++
-    this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
-    console.log(this.game.currentPlayer);
+    console.log(this.game.pickCardAnimation);
+    console.log(this.game.currentCard);
+
+
+
   }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogAddPlayerComponent);
-
-    dialogRef.afterClosed().subscribe(name => {
-      if(name && name.length > 0) {
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if (name && name.length > 0) {
         this.game.players.push(name);
+        this.saveGame();
       }
     });
   }
 
-  
+  saveGame() {
+    console.log('game', this.game.toJson());
+
+    this.firestore
+      .collection('games')
+      .doc(this.gameId)
+      .update(this.game.toJson());
+
+  }
+
+
 }
 
