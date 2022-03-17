b2c0d81459e4f2bffb7f47ecef7c1b0b03e1fd75
Aldin <aldin.bijedic@gmx.at>
2022-03-17 11:59:40 +0100

    Update dialog-add-player.component.ts

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Game } from 'src/models/game';
 
-
+export interface DialogData {
+  selectedValue: string;
+  name: string;
+}
 @Component({
   selector: 'app-dialog-add-player',
   templateUrl: './dialog-add-player.component.html',
   styleUrls: ['./dialog-add-player.component.scss']
 })
 
-
-
 export class DialogAddPlayerComponent implements OnInit {
 
   name: string = '';
-  
-  constructor(private dialog: MatDialog, public dialogRef: MatDialogRef<DialogAddPlayerComponent>) { }
+  selectedValue: string = '';
+  game = new Game();
+
+  constructor(private dialog: MatDialog,
+    public dialogRef: MatDialogRef<DialogAddPlayerComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
   ngOnInit(): void {
   }
 
-  onNoClick(): void{
+  onNoClick(): void {
     this.dialogRef.close();
   }
 
