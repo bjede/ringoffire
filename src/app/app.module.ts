19ef3fa3ebb6f5cab218dbcf939aa270188f42c0
Aldin <aldin.bijedic@gmx.at>
2022-03-17 11:58:30 +0100

    Import of some components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
@@ -8,23 +7,22 @@ import { GameComponent } from './game/game.component';
 import { PlayerComponent } from './player/player.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-
-
-
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule} from '@angular/material/input';
 import { GameInfoComponent } from './game-info/game-info.component';
-import {MatCardModule} from '@angular/material/card';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { MatCardModule } from '@angular/material/card';
 import { environment } from '../environments/environment';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
-
-
+import { PlayerMobileComponent } from './player-mobile/player-mobile.component';
+import { EditPlayerComponent } from './edit-player/edit-player.component';
+import { MatSelectModule } from '@angular/material/select';
+import { DialogAddPlayerMessageComponent } from './dialog-add-player-message/dialog-add-player-message.component';
+import { GameOverComponent } from './game-over/game-over.component'; 
 
 @NgModule({
   declarations: [
@@ -33,7 +31,11 @@ import { AngularFireModule } from '@angular/fire/compat';
     GameComponent,
     PlayerComponent,
     DialogAddPlayerComponent,
-    GameInfoComponent
+    GameInfoComponent,
+    PlayerMobileComponent,
+    EditPlayerComponent,
+    DialogAddPlayerMessageComponent,
+    GameOverComponent
   ],
   imports: [
     BrowserModule,
@@ -45,6 +47,7 @@ import { AngularFireModule } from '@angular/fire/compat';
     FormsModule,
     MatInputModule,
     MatCardModule,
+    MatSelectModule,
     AngularFireModule.initializeApp(environment.firebase),
     provideDatabase(() => getDatabase()),
     provideFirestore(() => getFirestore())
