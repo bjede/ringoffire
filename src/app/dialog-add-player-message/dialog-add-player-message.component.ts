458d1fe1b769a2103226238782dd7fbec992d95f
Aldin <aldin.bijedic@gmx.at>
2022-03-17 11:59:21 +0100

    Create dialog-add-player-message.component.ts

---
+++ src/app/dialog-add-player-message/dialog-add-player-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-add-player-message',
+  templateUrl: './dialog-add-player-message.component.html',
+  styleUrls: ['./dialog-add-player-message.component.scss']
+})
+export class DialogAddPlayerMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
